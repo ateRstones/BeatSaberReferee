c0ec609b51a9e84f45771d3091d871b28a90b440
ateRstones <t.schmolzi@web.de>
2019-12-09 14:15:27 +0100

    Added accept command for requests

---
+++ src/bot-commands/RequestAcceptCommand.ts
@@ -0,0 +1,69 @@
+import { Message, Role, GuildMember } from "discord.js"
+import ActivatedCommand from "../models/ActivatedCommand"
+import Command from "./Command"
+import LocalizedMessage from "../utils/LocalizedMessage"
+import Logger from "../utils/logging"
+import { database } from "../index"
+import DuelRequest from "../models/database/entity/DuelRequest"
+import Duel from "../models/database/entity/Duel"
+
+let logger = Logger.getLogger("beatsaber")
+
+export default class RequestCommand extends Command {
+
+    public name: string = "accept"
+    public description: string = "Command for accepting duels."
+    public async execute(activatedCommand: ActivatedCommand): Promise<void> {
+
+        if(activatedCommand.args.length > 0) {
+            let input = activatedCommand.args.join(" ")
+            
+            let challengingMember: GuildMember;
+            if(input.startsWith("<@")) {
+                let id = input.substring(2, input.length - 1)
+                challengingMember = activatedCommand.message.guild.members.find(m => m.id === id)
+            } else {
+                challengingMember = activatedCommand.message.guild.members.find(m => m.displayName === input)
+            }
+
+            if(challengingMember !== undefined) {
+                let acceptingMember = activatedCommand.message.member
+                if(challengingMember.id !== acceptingMember.id) {
+                    let challengingUser = await database.getOrCreateUpdatedUser(challengingMember.id, challengingMember.displayName)
+                    let acceptingUser = await database.getOrCreateUpdatedUser(acceptingMember.id, acceptingMember.displayName)
+
+                    if(!await acceptingUser.hasDuelWith(challengingUser)) {
+                        if(!await acceptingUser.hasRequested(challengingUser)) {
+                            if(await acceptingUser.gotRequestedBy(challengingUser)) {
+                                let acceptedDuel = new Duel()
+                                acceptedDuel.participants = [challengingUser, acceptingUser]
+                                acceptedDuel.save()
+                                activatedCommand.replyToChannel(LocalizedMessage.getParsed("commands.accept.msg.success", {"player": input}))
+                                let request = await DuelRequest.findOne({"requestingUser": challengingUser, "receivingUser": acceptingUser})
+                                if(request !== undefined) {
+                                    DuelRequest.remove(<DuelRequest>request)
+                                }
+                            } else {
+                                activatedCommand.replyToChannel(LocalizedMessage.getParsed("commands.accept.msg.norequest", {"player": input}))
+                            }
+                        } else {
+                            activatedCommand.replyToChannel(LocalizedMessage.getParsed("commands.accept.msg.requested", {"player": input}))
+                        }
+                    } else {
+                        activatedCommand.replyToChannel(LocalizedMessage.getParsed("commands.accept.msg.alreadyhasduel", {"player": input}))
+                    }
+                } else {
+                    activatedCommand.replyToChannel(LocalizedMessage.getLocalized("commands.accept.error.selfduel"))
+                }
+                
+            } else {
+                activatedCommand.replyToChannel(LocalizedMessage.getParsed("commands.accept.error.unknownplayer", {"player": input}))                
+            }
+            
+        } else {
+            activatedCommand.replyToChannel(LocalizedMessage.getLocalized("commands.accept.error.noargs"))
+        }
+    }
+
+
+}
--- src/settings/Settings.ts
+++ src/settings/Settings.ts
@@ -4,6 +4,7 @@ import Module from '../bot-modules/modules/Module'
 import CommandManagerModule from '../bot-modules/modules/CommandManagerModule'
 import ColorChangeCommand from '../bot-commands/ColorChangeCommand'
 import RequestCommand from '../bot-commands/RequestCommand'
+import RequestAcceptCommand from '../bot-commands/RequestAcceptCommand'
 
 class BeatsaberRefereeBotSettings {
 
@@ -13,7 +14,8 @@ class BeatsaberRefereeBotSettings {
 
     public readonly commands: Command[] = [
         new ColorChangeCommand(c.get<string[]>("discord.colorRoles")),
-        new RequestCommand()
+        new RequestCommand(),
+        new RequestAcceptCommand()
     ]
 
     public readonly prefix = c.get<string>('discord.prefix')
